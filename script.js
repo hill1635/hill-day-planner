35c645c02b76e3461bf09eea9caecd8d19d86603
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-04-05 13:17:50 -0600

    Reformatted for loop, changed variable names.

--- script.js
+++ script.js
@@ -14,19 +14,40 @@ $(document).ready(function () {
   init();
 
   for (var i = 0; i < 9; i++) {
-    var dataTime = $("#hour-" + [i]).attr("data-time");
-    var nextIndex = [i + 1];
-    var nextTime = $("#hour-" + nextIndex).attr("data-time");
-    var textArea = $("#text-" + [i]);
+    var div = this.children[0].children[1].children[1].children[0].children[i];
+    var nextDiv = this.children[0].children[1].children[1].children[0].children[i + 1];
+    var textInput = div.children[1];
+    var btn = div.children[2];
+    var hour = $(div).attr("data-time");
+    var nextHour = $(nextDiv).attr("data-time");
 
-    if (time >= dataTime && time < nextTime) {
-      $(textArea).attr("class", "present");
-    } else if (time > dataTime) {
-      $(textArea).attr("class", "past");
-    } else if (time < dataTime) {
-      $(textArea).attr("class", "future");
+    if (time >= hour && time < nextHour) {
+      $(textInput).attr("class", "present");
+    } else if (time > hour) {
+      $(textInput).attr("class", "past");
+    } else if (time < hour) {
+      $(textInput).attr("class", "future");
     }
+    // console.log("nextDiv: ", nextDiv);
+    // console.log("textInput: ", textInput);
+    // console.log("btn: ", btn);
+    // console.log("hour: ", hour);
   }
+
+  // for (var i = 0; i < 9; i++) {
+  //   var dataTime = $("#hour-" + [i]).attr("data-time");
+  //   var nextIndex = [i + 1];
+  //   var nextTime = $("#hour-" + nextIndex).attr("data-time");
+  //   var textArea = $("#text-" + [i]);
+
+  //   if (time >= dataTime && time < nextTime) {
+  //     $(textArea).attr("class", "present");
+  //   } else if (time > dataTime) {
+  //     $(textArea).attr("class", "past");
+  //   } else if (time < dataTime) {
+  //     $(textArea).attr("class", "future");
+  //   }
+  // }
 
   for (var i = 0; i < 9; i++) {
     $("#btn-" + [i]).on("click", function () {
